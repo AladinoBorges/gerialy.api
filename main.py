388772676ccbc2e73897d2137653175cfe97009f
aladino_borges <eualadino@gmail.com>
2025-10-08 20:38:57 -0300

    feat: implements app life cycles

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
+from connections import DATABASE_ENGINE
 from routers import status
 
-app = FastAPI()
 
-app.include_router(status.router)
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
+
+    DATABASE_ENGINE.dispose()
+
+
+main_app = FastAPI(lifespan=lifespan)
+main_app.include_router(status.router)
 
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(main_app, host="0.0.0.0", port=8000)
